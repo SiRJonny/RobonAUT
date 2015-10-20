31ae51486a0e0fefd7240774e73c989e17ada985
magab89 <magab89@gmail.com>
2015-10-20 22:40:13 +0200

    void test() függvény

--- include/bt_stress.h
+++ include/bt_stress.h
@@ -3,8 +3,10 @@
 #ifndef INCLUDE_BT_STRESS_H_
 #define INCLUDE_BT_STRESS_H_
 
+#include "cmsis_os.h"
+#include "BT_MSG.h"
 
-
+void test();
 
 
 
--- src/bt_stress.c
+++ src/bt_stress.c
@@ -4,9 +4,30 @@
  *  Created on: 2015. okt. 18.
  *      Author: Gabor
  */
+#include "bt_stress.h"
+
+
+extern QueueHandle_t xQueue_BT;
+
+void test() {
+
+
+	struct BT_MSG msg_int;
+	struct BT_MSG * msg_int_ptr = &msg_int;
+
+	struct BT_MSG msg_int_arr[5];
+
+	for (int i = 0; i < 5; i++) {
+		msg_int_ptr = &msg_int_arr[i];
+		int2msg(msg_int_ptr, i, "iii_int32\n");
+		xQueueSend( xQueue_BT, (void*) &msg_int_ptr, osWaitForever);
+
+	}
 
 
 
+
+}
 
 
 
