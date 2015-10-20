cd130e1698924083d3030b3cf0b92f5d5135386b
magab89 <magab89@gmail.com>
2015-10-20 20:51:54 +0200

    BT stresszteszthez

---
+++ include/bt_stress.h
@@ -0,0 +1,13 @@
+
+
+#ifndef INCLUDE_BT_STRESS_H_
+#define INCLUDE_BT_STRESS_H_
+
+
+
+
+
+
+
+
+#endif /* INCLUDE_BT_STRESS_H_ */
---
+++ src/bt_stress.c
@@ -0,0 +1,16 @@
+/*
+ * bt_stress.c
+ *
+ *  Created on: 2015. okt. 18.
+ *      Author: Gabor
+ */
+
+
+
+
+
+
+
+
+
+
